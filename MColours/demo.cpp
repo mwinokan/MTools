c0d0983fe232bc9dda801cb482f94efd9b6b804f
mwinokan <mwinokan@me.com>
2018-11-30 14:15:55 -0800

    Updated to reflect latest changes.

--- MColours/demo.cpp
+++ MColours/demo.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main() {
 	
   cout << colBold << "colBold (1m)" << colClear << endl;
-	cout << colVarName << "colVarName (36m)" << colClear << endl;
+  cout << colVarName << "colVarName (36m)" << colClear << endl;
   cout << colVarType << "colVarType (95m)" << colClear << endl;
   cout << colFunc << "colFunc (36;4m)" << colClear << endl;
   cout << colFile << "colFile (38;5;11m)" << colClear << endl;
   cout << colError << "colError (31;1m)" << colClear << endl;
   cout << colSuccess << "colSuccess (32;1m)" << colClear << endl;
-  cout << colResult << "colResult (34m)" << colClear << endl;
+  cout << colResult << "colResult (38;5;27m)" << colClear << endl;
   cout << colWarning << "colWarning (38;5;166m)" << colClear << endl;
   cout << colArg << "colArg (38;5;154m)" << colClear << endl;
   cout << colInverse << "colInverse (34m)" << colClear << endl;
